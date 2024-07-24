ae2b51914a332f7dbc9bd9e35d4558c9ba88dcea
huang-jenny <jennyyyhuang@yahoo.de>
2024-07-24 12:10:00 +0200

    show past events only if there are any

--- src/pages/eventsPage/EventsPage.jsx
+++ src/pages/eventsPage/EventsPage.jsx
@@ -22,7 +22,6 @@ import { COLOR_BACKGROUND_LIGHT, COLOR_TEXT_SECONDARY } from '../../constants/st
 
 const EventsPage = () => {
   const [eventsContent, setEventsContent] = useState();
-  const [eventsList, setEventsList] = useState();
   const [previousEvents, setPreviousEvents] = useState([]);
   const [upcomingEvents, setUpcomingEvents] = useState([]);
 
@@ -35,10 +34,6 @@ const EventsPage = () => {
       );
     });
   }, []);
-
-  useEffect(() => {
-    console.log(previousEvents);
-  }, [previousEvents]);
 
   return (
     <PageWrapper>
@@ -129,81 +124,86 @@ const EventsPage = () => {
       <Box py={8}>{renderMarkdown(eventsContent)}</Box>
       <Box h={[6, 7, 8]} />
       {/* PAST EVENTS */}
-      <Title name="Vergangene Veranstaltungen" textAlign="left" />
+      {previousEvents.length > 0 && (
+        <>
+          <Title name="Vergangene Veranstaltungen" textAlign="left" />
 
-      <Wrap w="100%" spacing={0}>
-        <MobileView>
-          <TableContainer w="100%" whiteSpace="normal" display="container" overflowX="auto">
-            <Table variant="striped" colorScheme="whiteAlpha">
-              <Tbody>
-                {previousEvents &&
-                  previousEvents.map((event) => (
-                    <Tr key={v4()}>
-                      <Td px={3} py={3}>
-                        <VStack align="stretch">
-                          <Box fontWeight="thin">
+          <Wrap w="100%" spacing={0}>
+            <MobileView>
+              <TableContainer w="100%" whiteSpace="normal" display="container" overflowX="auto">
+                <Table variant="striped" colorScheme="whiteAlpha">
+                  <Tbody>
+                    {previousEvents.map((event) => (
+                      <Tr key={v4()}>
+                        <Td px={3} py={3}>
+                          <VStack align="stretch">
+                            <Box fontWeight="thin">
+                              {formatDate(
+                                event.attributes.event_start,
+                                event.attributes.event_end,
+                                event.attributes.include_day,
+                                event.attributes.include_time
+                              )}
+                            </Box>
+                            <Box fontWeight="bold">{event.attributes.title}</Box>
+                            {event.attributes.shortDescription && (
+                              <Box>{event.attributes.shortDescription}</Box>
+                            )}
+                            {event.attributes.location && (
+                              <Box>Ort: {event.attributes.location}</Box>
+                            )}
+                          </VStack>
+                        </Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </MobileView>
+            <BrowserView>
+              <TableContainer w="100%" whiteSpace="normal">
+                <Table variant="striped" colorScheme="whiteAlpha">
+                  <Thead>
+                    <Tr>
+                      <Th w="20%" color={COLOR_TEXT_SECONDARY}>
+                        Wann
+                      </Th>
+                      <Th w="60%" color={COLOR_TEXT_SECONDARY}>
+                        Event
+                      </Th>
+                      <Th w="20%" color={COLOR_TEXT_SECONDARY}>
+                        Wo
+                      </Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {previousEvents &&
+                      previousEvents.map((event) => (
+                        <Tr key={v4()}>
+                          <Td p={3}>
                             {formatDate(
                               event.attributes.event_start,
                               event.attributes.event_end,
                               event.attributes.include_day,
                               event.attributes.include_time
                             )}
-                          </Box>
-                          <Box fontWeight="bold">{event.attributes.title}</Box>
-                          {event.attributes.shortDescription && (
-                            <Box>{event.attributes.shortDescription}</Box>
-                          )}
-                          {event.attributes.location && <Box>Ort: {event.attributes.location}</Box>}
-                        </VStack>
-                      </Td>
-                    </Tr>
-                  ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </MobileView>
-        <BrowserView>
-          <TableContainer w="100%" whiteSpace="normal">
-            <Table variant="striped" colorScheme="whiteAlpha">
-              <Thead>
-                <Tr>
-                  <Th w="20%" color={COLOR_TEXT_SECONDARY}>
-                    Wann
-                  </Th>
-                  <Th w="60%" color={COLOR_TEXT_SECONDARY}>
-                    Event
-                  </Th>
-                  <Th w="20%" color={COLOR_TEXT_SECONDARY}>
-                    Wo
-                  </Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {previousEvents &&
-                  previousEvents.map((event) => (
-                    <Tr key={v4()}>
-                      <Td p={3}>
-                        {formatDate(
-                          event.attributes.event_start,
-                          event.attributes.event_end,
-                          event.attributes.include_day,
-                          event.attributes.include_time
-                        )}
-                      </Td>
-                      <Td>
-                        <VStack align="stretch">
-                          <Box fontWeight="bold">{event.attributes.title}</Box>
-                          <Box fontWeight="light">{event.attributes.shortDescription}</Box>
-                        </VStack>
-                      </Td>
-                      <Td>{event.attributes.location}</Td>
-                    </Tr>
-                  ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </BrowserView>
-      </Wrap>
+                          </Td>
+                          <Td>
+                            <VStack align="stretch">
+                              <Box fontWeight="bold">{event.attributes.title}</Box>
+                              <Box fontWeight="light">{event.attributes.shortDescription}</Box>
+                            </VStack>
+                          </Td>
+                          <Td>{event.attributes.location}</Td>
+                        </Tr>
+                      ))}
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </BrowserView>
+          </Wrap>
+        </>
+      )}
     </PageWrapper>
   );
 };
